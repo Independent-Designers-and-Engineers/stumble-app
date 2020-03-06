c0887b92e7c46a28b2e3c7bd0dfab44424ea22be
Adam <ahomais@hotmail.com>
2020-03-06 15:24:37 -0700

    Created theme file

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final white = Color(0xFFFFFFFF);
+final coral = Color(0xFFFF866E);
+var maintheme = ThemeData(
+  primaryColor: white,
+  accentColor: coral,
+  fontFamily: 'Raleway',
+);
